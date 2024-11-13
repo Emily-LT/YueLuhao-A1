2b6b31cce719d5a886a42fe3c412e8b3239305ed
lyue11 <152278981+Emily-LT@users.noreply.github.com>
2024-11-13 11:12:11 +0800

    创建 OtherHealthProfessional.java 文件

---
+++ src/OtherHealthProfessional.java
@@ -0,0 +1,30 @@
+public class OtherHealthProfessional extends HealthProfessional {
+    // 实例变量
+    private String certification;
+
+    // 默认构造函数
+    public OtherHealthProfessional() {
+    }
+
+    // 第二个构造函数，用于初始化所有实例变量
+    public OtherHealthProfessional(int ID, String name, String specialization, String certification) {
+        super(ID, name, specialization);
+        this.certification = certification;
+    }
+
+    // 打印健康专业人员的详细信息的方法
+    @Override
+    public void printDetails() {
+        super.printDetails();
+        System.out.println("Certification: " + certification);
+    }
+
+    // Getter 和 Setter 方法
+    public String getCertification() {
+        return certification;
+    }
+
+    public void setCertification(String certification) {
+        this.certification = certification;
+    }
+}

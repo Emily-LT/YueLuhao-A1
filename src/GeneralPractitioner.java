f87a92c2d978a632226168829f0ca5fe307aa1fc
lyue11 <152278981+Emily-LT@users.noreply.github.com>
2024-11-18 13:23:47 +0800

    Modified general practitioner class

--- src/GeneralPractitioner.java
+++ src/GeneralPractitioner.java
@@ -1,22 +1,17 @@
 public class GeneralPractitioner extends HealthProfessional {
-    // 实例变量
-    private int experienceYears;
+    // 这是实例变量（全科医生）
+    private String clinic;
 
-    // 默认构造函数
+    // 默认构造方法
     public GeneralPractitioner() {
+        super(); // 调用父类的默认构造方法
+        this.clinic = "";
     }
 
     // 第二个构造函数，用于初始化所有实例变量
-    public GeneralPractitioner(int ID, String name, String specialization, int experienceYears) {
-        super(ID, name, specialization);
-        this.experienceYears = experienceYears;
-    }
-
-    // 打印健康专业人员的详细信息的方法
-    @Override
-    public void printDetails() {
-        super.printDetails();
-        System.out.println("Experience Years: " + experienceYears);
+    public GeneralPractitioner(int ID, String name, String specialty, String clinic) {
+        super(ID, name, specialty); // 调用父类的带参数构造方法
+        this.clinic = clinic;
     }
 
     // Getter 和 Setter 方法
